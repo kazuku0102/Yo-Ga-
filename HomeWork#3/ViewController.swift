92c383f7479bfe5cad5242c28c57c535f51e191f
Shelly <shelly980102@gmail.cim>
2019-12-24 19:21:43 +0800

    Initial Commit

---
+++ HomeWork#3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork#3
+//
+//  Created by 蕭聿莘 on 2019/12/24.
+//  Copyright © 2019 Kazuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
